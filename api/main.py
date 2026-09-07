dab96cda4cd2b7f835ff967c1d101ee3f1026134
Yash Rathi <rathiyash2005@gmail.com>
2026-09-07 23:39:47 +0530

    Allow Vercel frontend origin

--- api/main.py
+++ api/main.py
@@ -66,10 +66,10 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://business-investigator.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
 )
 
 
